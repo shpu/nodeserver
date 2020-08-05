c5f2d3ad96c74a90519f58fd4d348ad75aae47fa
shpu <67029600+shpu@users.noreply.github.com>
2020-08-05 18:27:43 +0300

    1

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const app = express();
+
+let anime = [
+    {
+        id: 1,
+        name: 'Boku no Pico'
+    },
+    {
+        id:2,
+        name: 'Fok Fok Literature Club'
+    },
+    {
+        id:3,
+        name: 'Dick'
+    }
+];
+
+app.get('/', function (req, res) {
+    res.send('OK');
+})
+
+app.get('/anime', function (req, res) {
+    res.send(anime);
+})
+
+app.listen(3000, function () {
+   console.log('Server -- OK');
+})

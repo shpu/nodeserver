351e29679fff7a078388e05562c2e0e08a1e1585
shpu <67029600+shpu@users.noreply.github.com>
2020-08-06 15:32:49 +0300

    Realesed connection to MongoDB

--- index.js
+++ index.js
@@ -5,43 +5,52 @@ const MongoClient = require('mongodb').MongoClient;
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+const url =  'mongodb+srv://admin:nopassword@morgtown-data-cluster.dasjj.mongodb.net/morgtown_chat';
+let db;
 
-let anime = [
-    {
-        id: 1,
-        name: 'Boku no Pico'
-    },
-    {
-        id: 2,
-        name: 'Fok Fok Literature Club'
-    },
-    {
-        id: 3,
-        name: 'Dick'
-    },
-    {
-        id: 4,
-        name: 'Van'
+MongoClient.connect(url, function (err, client) {
+    if (err) {
+        console.log(err);
     }
-];
+    console.log('DB ---- OK')
+}) //подключение к БД
 
-app.get('/', function (req, res) {
-    res.send('OK');
-})
-
-app.get('/anime', function (req, res) {
-    res.send(anime);
-})
-
-app.get('/anime/:id', function (req, res) {
-    console.log(req.params);
-    var anima = anime.find(function (anima)
-    {
-        return anima.id === Number(req.params.id)
-    });
-    res.send(anima);
-})
-
-app.listen(3000, function () {
-   console.log('Server -- OK');
-})
+// let anime = [
+//     {
+//         id: 1,
+//         name: 'Boku no Pico'
+//     },
+//     {
+//         id: 2,
+//         name: 'Fok Fok Literature Club'
+//     },
+//     {
+//         id: 3,
+//         name: 'Dick'
+//     },
+//     {
+//         id: 4,
+//         name: 'Van'
+//     }
+// ];
+//
+// app.get('/', function (req, res) {
+//     res.send('OK');
+// })
+//
+// app.get('/anime', function (req, res) {
+//     res.send(anime);
+// })
+//
+// app.get('/anime/:id', function (req, res) {
+//     console.log(req.params);
+//     var anima = anime.find(function (anima)
+//     {
+//         return anima.id === Number(req.params.id)
+//     });
+//     res.send(anima);
+// })
+//
+// app.listen(3000, function () {
+//    console.log('Server -- OK');
+// })
